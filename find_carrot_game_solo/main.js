731c3fa9d80f0b0fad24b0f4b9e1bbe8c235dfa0
김용민 <dydals3440@gmail.com>
2023-02-21 16:07:21 +0900

    JS: Refresh Btn

--- find_carrot_game_solo/main.js
+++ find_carrot_game_solo/main.js
@@ -22,6 +22,7 @@ let score = 0;
 
 function initGame() {
   field.innerText = "";
+  gameScore.innerHTML = CARROT_COUNT;
   addItem("carrot", CARROT_COUNT, "./img/carrot.png");
   addItem("bug", BUG_COUNT, "./img/bug.png");
 }
@@ -35,12 +36,52 @@ gameBtn.addEventListener("click", () => {
   started = !started; // 이처리를 해줘야지 시작했으면 => 다시 시작했다고 변경해주어야함
 });
 
+field.addEventListener("click", (e) => {
+  // 시작되지 않았으면 리턴
+  if (!started) {
+    return;
+  }
+  const target = e.target;
+  if (target.matches(".carrot")) {
+    target.remove();
+    // 점수 증가
+    score++;
+    // 그 후 UI에 업데이트
+    updateScore();
+    // 1. 종료조건(당근을 다 먹었을때) => true
+    if (score === CARROT_COUNT) {
+      finishGame();
+    }
+  } else if (target.matches(".bug")) {
+    // 2. 종료조건(벌레를 클릭했을때) =>
+    stopGameTimer();
+    finishGame(false);
+  }
+}); // finishgame을 true나 false로 정의하는 것은 사실 좋은 코드가 아니랍니다.
+
+// 3. 종료조건 (타이머 끝날때)
+
+popUpBtn.addEventListener("click", () => {
+  // 게임 시작 => 팝업창 없앰 순서
+  gameStart();
+  hidePopUpAndText();
+  showStopButton();
+});
+
+// 승패 여부를 인자로
+function finishGame(win) {
+  // 1. 게임이 끝났다는것은 종료했다는 의미
+  started = false;
+  // 2. gamebtn 안보이게
+  hideGameButton();
+  showPopUpAndText(win ? "YOU WIN" : "YOU LOST");
+}
+
 function gameStart() {
   initGame();
   showStopButton();
   showTimerAndScore();
   startGameTimer();
-  showPopUpAndText();
 }
 
 function gameStop() {
@@ -64,13 +105,19 @@ function showTimerAndScore() {
   gameScore.style.visibility = "visible";
 }
 
+function updateScore() {
+  gameScore.innerText = CARROT_COUNT - score;
+}
+
 function startGameTimer() {
   let remainingTime = GAME_DURATION_SEC;
   updateTimerText(remainingTime);
   timer = setInterval(() => {
     if (remainingTime <= 0) {
-      // 타이머 종료 후 더이상 진행되지 않게 return
+      // 타이머 종료 후 더이상 진행되지 않게 return 게임 종료도 이루어져야함(3번 종료조건)
       clearInterval(timer);
+      // 승패 여부는 당근을 다 못먹었냐로 정해짐
+      finishGame(CARROT_COUNT === score);
       return;
     }
     updateTimerText(--remainingTime);
@@ -88,8 +135,13 @@ function updateTimerText(time) {
 }
 
 function showPopUpAndText(text) {
+  // 아니면 나중에 pop-up--hide 달아놓고 나중에 remove ㄱㄱ
   popUp.style.visibility = "visible";
   popUpMessage.innerText = text;
+}
+
+function hidePopUpAndText(text) {
+  popUp.style.visibility = "hidden";
 }
 
 // 아이템을 추가
